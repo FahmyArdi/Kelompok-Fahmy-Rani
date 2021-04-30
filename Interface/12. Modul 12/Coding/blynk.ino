072d22be273df39b667eee436447ac40e16a66ae
FahmyArdi <63042444+FahmyArdi@users.noreply.github.com>
2021-04-30 19:18:12 +0700

    Edit

---
+++ Interface/12. Modul 12/Coding/blynk.ino
@@ -0,0 +1,32 @@
+/*************************************************************** 
+*  Aplikasi kontrol LED via Blynk Server 
+* Buka command windows pada direktori berikut  
+* C:\Users\Username\Documents\Arduino\libraries\Blynk\scripts 
+ *  
+* Pada command prompt ketikan: blynk-ser.bat -c COM2 lalu enter  *  COM2 disesuaikan dengan port yang digunakan pada Proteus Anda! 
+ 
+****************************************************************/ 
+#include <BlynkSimpleStream.h> 
+ 
+// Pin Assignments 
+int redPin=13,yellowPin=12,greenPin=11,bluePin=10; 
+ 
+//Ganti Auth Token sesuai kode yang muncul pada aplikasi Blynk pada 
+//smartphone Anda. Auth Token di bawah ini hanyalah contoh. 
+char auth[] = "3EspxFanL0TmaFMwml9ovMfeY9N7skSS"; 
+ 
+void setup() { 
+  //Atur pin sebagai output   
+  pinMode(redPin,OUTPUT);   
+  pinMode(yellowPin,OUTPUT);   
+  pinMode(greenPin,OUTPUT);   
+  pinMode(bluePin,OUTPUT); 
+ 
+  // Blynk berkomunikasi melalui komunikasi serial 
+  Serial.begin(9600); 
+  Blynk.begin(auth, Serial); 
+}  
+void loop() 
+{ 
+//mengaktifkan fungsi Blynk 
+  Blynk.run(); } 
